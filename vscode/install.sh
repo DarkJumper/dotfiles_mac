0e63d233a59be05ded77966f27fe142d04a0bf26
Peter Schwarz <p.schwarz1994@outlook.de>
2021-07-04 13:14:04 +0200

    new keybinds and extensions

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,22 +1,63 @@
-code --install-extension batisteo.vscode-django
-code --install-extension bigonesystems.django
-code --install-extension mhutchie.git-graph
-code --install-extension knisterpeter.vscode-github
-code --install-extension github.vscode-pull-request-github
-code --install-extension eamodio.gitlens
-code --install-extension ms-python.gather
-code --install-extension ms-toolsai.jupyter
-code --install-extension ms-python.vscode-pylance
-code --install-extension ms-python.python
-code --install-extension ahmadawais.shades-of-purple
-code --install-extension emmanuelbeziat.vscode-great-icons
-code --install-extension coenraads.bracket-pair-colorizer-2
-code --install-extension alexdima.copy-relative-path
-code --install-extension alefragnani.bookmarks
-code --install-extension ms-vscode.cpptools
-code --install-extension ms-vscode.vs-keybindings
-code --install-extension skyapps.fish-vscode
-code --install-extension skyapps.fish-vscode
-code --install-extension njpwerner.autodocstring
-code --install-extension visualstudioexptteam.vscodeintellicode
-code --install-extension formulahendry.code-runner
+{
+    "workbench.settings.editor": "json",
+    "workbench.settings.openDefaultSettings": true,
+    "workbench.settings.useSplitJSON": true,
+    "workbench.iconTheme": "vscode-great-icons",
+    "autoDocstring.docstringFormat": "google",
+    "autoDocstring.includeExtendedSummary": false,
+    "autoDocstring.includeName": true,
+    "autoDocstring.startOnNewLine": true,
+    "editor.wordWrapColumn": 120,
+    "editor.wordWrap": "on",
+    "python.pythonPath": "//Users/peterschwarz/.pyenv/shims/python",
+    "editor.formatOnSave": true,
+    "python.formatting.provider": "yapf",
+    "editor.fontSize": 15,
+    "editor.fontWeight": "500",
+    "editor.fontFamily": "Inter",
+    "debug.console.fontFamily": "Inter",
+    "debug.console.fontSize": 14,
+    "terminal.integrated.fontSize": 14,
+    "terminal.integrated.fontWeight": "600",
+    "python.venvPath": "venv//bin//python3",
+    "editor.formatOnPaste": true,
+    "terminal.integrated.fontFamily": "monospace",
+    "code-runner.executorMap": {
+        "python": "$pythonPath -u $fullFileName"
+    },
+    "[plaintext]": {
+        "editor.quickSuggestions": false
+    },
+    "editor.quickSuggestionsDelay": 100,
+    "zenMode.centerLayout": false,
+    "zenMode.fullScreen": false,
+    "zenMode.hideLineNumbers": false,
+    "zenMode.hideTabs": false,
+    "editor.minimap.enabled": false,
+    "git.enableSmartCommit": true,
+    "python.formatting.yapfPath": "yapf",
+    "python.formatting.yapfArgs": [
+        "--style",
+        "{based_on_style:pep8, ARITHMETIC_PRECEDENCE_INDICATION:True,BLANK_LINE_BEFORE_NESTED_CLASS_OR_DEF: True,COLUMN_LIMIT:120,INDENT_WIDTH:4,INDENT_CLOSING_BRACKETS: True,NO_SPACES_AROUND_SELECTED_BINARY_OPERATORS:set(*,/),SPACES_BEFORE_COMMENT:15,20}"
+    ],
+    "python.linting.pylintArgs": [
+        "--max-line-length",
+        "80",
+        "--aggressive"
+    ],
+    "python.languageServer": "Pylance",
+    "explorer.confirmDelete": false,
+    "gitlens.views.repositories.files.layout": "list",
+    "workbench.colorTheme": "LaserWave",
+    "C_Cpp.updateChannel": "Insiders",
+    "jupyter.askForKernelRestart": false,
+    "editor.suggestSelection": "first",
+    "vsintellicode.modify.editor.suggestSelection": "automaticallyOverrodeDefaultValue",
+    "jupyter.textOutputLimit": 2500,
+    "workbench.editorAssociations": {
+        "*.ipynb": "jupyter.notebook.ipynb"
+    },
+    "kite.showWelcomeNotificationOnStartup": false,
+    "window.zoomLevel": 1,
+    "python.defaultInterpreterPath": "//Users/peterschwarz/.pyenv/shims/python"
+}
