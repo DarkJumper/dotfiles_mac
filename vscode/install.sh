2064da571f0b454b27570960fde7141ef9b13e22
Peter Schwarz <p.schwarz1994@outlook.de>
2021-11-16 15:26:46 +0100

    new settings!

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,21 +1,51 @@
+#Keybindings Intellj
+code -- install-extension k--kato.intellij-idea-keybindings
+# Django syntax highlighting
 code --install-extension batisteo.vscode-django
-code --install-extension bigonesystems.django
-code --install-extension bibhasdn.django-snippets
+# Django templates
 code --install-extension bibhasdn.django-html
+# git Graph 
 code --install-extension mhutchie.git-graph
-code --install-extension felipecaputo.git-project-manager
-code --install-extension knisterpeter.vscode-github
-code --install-extension github.vscode-pull-request-github
+# git lens
 code --install-extension eamodio.gitlens
+# python gather for jupyter
 code --install-extension ms-python.gather
+# Jupyter
 code --install-extension ms-toolsai.jupyter
+# Pylance
 code --install-extension ms-python.vscode-pylance
+# Python language support
 code --install-extension ms-python.python
-code --install-extension ahmadawais.shades-of-purple
+# icons theme
 code --install-extension emmanuelbeziat.vscode-great-icons
+# ide theme
+code --isntall-extension github.github-vscode-theme
+# Bracked color
 code --install-extension coenraads.bracket-pair-colorizer-2
+# Relativ path with right click
 code --install-extension alexdima.copy-relative-path
+# Bockmarks in code
 code --install-extension alefragnani.bookmarks
+# CPP / C
 code --install-extension ms-vscode.cpptools
-code --install-extension ms-vscode.vs-keybindings
+# fisch for vs code
 code --install-extension skyapps.fish-vscode
+# german language package
+code --install-extension ms-ceintl.vscode-language-pack-de
+# see tabs
+code --install-extension oderwat.indent-rainbow
+# rander grafic jupyter
+code --install-extension ms-toolsai.jupyter-renderers
+# live share
+code --isntall-extension ms-vsliveshare.vsliveshare
+# live share extension
+code --install-extension ms-vsliveshare.vsliveshare-pack
+# live share audio
+code --install-extension ms-vsliveshare.vsliveshare-audio
+# auto docstring
+code --install-extension njpwerner.autodocstring
+# sqllite
+code --install-extension alexcvzz.vscode-sqlite
+# autocomplete 
+code --install-extension tabnine.tabnine-vscode
+code --isntall-extension visualstudioexptteam.vscodein
